520d4f15bd735848bd61de94fb7d5a8f65d63258
Danya <danil51608@gmail.com>
2021-06-14 19:21:37 +0300

    Added draggable element

--- src/components/DesignArea/DesignArea.js
+++ src/components/DesignArea/DesignArea.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './DesignArea.css'
 import tShirtFront from '../../imgs/t-shirtFront.png'
 import tShirtBack from '../../imgs/t-shirtBack.png'
@@ -10,10 +10,9 @@ import {switchToBlue, switchToPurple, switchToGreen, switchToBlack, switchToRed,
 function DesignArea(props){
 
     const {switchToBlue, switchToPurple, switchToGreen, switchToBlack, switchToRed, switchToWhite, color, switchSide, side} = props
-
-    const handleClick = (e) =>{
-        e.target.classList.toggle('active')
-    }
+    // Make the DIV element draggable:
+    useEffect( () => dragElement(document.getElementById("draggable")), [])
+    
 
     return (
         <div className="DesignAreaWrapper">
@@ -28,6 +27,7 @@ function DesignArea(props){
                     !side ? <img src={tShirtBack} alt='' className={color}/> : null
 
                 }
+                <div id='draggable'></div>
             </div>
             <ul className='colorList'>
                 <li id='blueColor' onClick={switchToBlue}></li>
@@ -59,3 +59,44 @@ const mapDispatchToProps = {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(DesignArea)
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './settings.css'
 
 export default function Settings(){
+    const [hideSettings, setHideSettings] = useState(true)
     return(
         <div className="settings">
             <h2>Design You Product</h2>
-            <button className='pinkButton'>Show Printable Area</button>
+            {
+             hideSettings ? <button className='settingsButton' onClick={()=>setHideSettings(false)}>Show Printable Area</button> 
+             : <div>
+                 <button className='settingsButton' onClick={()=>setHideSettings(true)}>Hide Printable Area</button> 
+                 <button className='settingsButton'>Add Text</button> 
+                 <button className='settingsButton'>Add Image</button> 
+             </div> 
+            }
+
         </div>
     )
 }
